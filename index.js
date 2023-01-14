5d3170926695ad3b0a6319f98faea02ad323bddd
TimmieDevelops <79818585+TimmieDevelops@users.noreply.github.com>
2023-01-13 19:46:33 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const axios = require('axios');
+const readline = require('readline');
+const { it } = require('node:test');
+
+function generateJSON(shopData) {
+    // parse and organize data into JSON format
+    const jsonData = JSON.stringify(shopData);
+    // write data to a file
+    fs.writeFileSync('fortnite-shop.json', jsonData);
+    console.log('Fortnite shop JSON generated and written to fortnite-shop.json');
+}
+
+axios.get('https://fortnite-api.com/v2/store')
+    .then(response => {
+        // do something with the data
+        const shopData = response.data;
+        generateJSON(shopData);
+    })
+    .catch(error => {
+        console.log(error);
+    });
