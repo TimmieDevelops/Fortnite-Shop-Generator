027876c031241d95e31fe71e522375462fd51dbf
TimmieDevelops <79818585+TimmieDevelops@users.noreply.github.com>
2023-01-13 19:47:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function generateJSON(shopData) {
     console.log('Fortnite shop JSON generated and written to fortnite-shop.json');
 }
 
-axios.get('https://fortnite-api.com/v2/store')
+axios.get('https://api.nitestats.com/v1/epic/store')
     .then(response => {
         // do something with the data
         const shopData = response.data;
